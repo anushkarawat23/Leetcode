31bd4bd93207519600c33664e917d3c0ecdf69a5
Anushka Rawat <112893429+anushkarawat23@users.noreply.github.com>
2023-09-11 18:56:57 +0530

    Time: 2 ms (45.05%), Space: 6 MB (60.76%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -29,7 +29,6 @@ public:
 
         return dp[n];
     }
-*/
 
     // TABULATION METHOD
 
@@ -44,8 +43,30 @@ public:
         }
 
         return dp[n];
+    }
+*/
+    // TABULATION METHOD with SPACE OPTIMIZATION
+
+    int climbStairs(int n)
+    {
+        if( n <= 1)
+            return 1;
+        int prev1 = 1;
+        int prev2 = 1;
+        int curr;
+
+        for(int i = 2; i <= n ; i++)
+        {
+            curr = prev2 + prev1;
+            prev2 = prev1;
+            prev1 = curr;
+            
+        }
+
+        return curr;
 
     }
+    
 
 
 };
