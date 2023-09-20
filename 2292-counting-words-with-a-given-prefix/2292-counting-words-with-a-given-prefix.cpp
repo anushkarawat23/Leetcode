aa5c35414c7051801b15e82cb3a21ae1258195fb
Anushka Rawat <112893429+anushkarawat23@users.noreply.github.com>
2023-09-20 17:40:00 +0530

    Time: 8 ms (79.75%), Space: 10.1 MB (77.35%) - LeetHub

--- 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.cpp
+++ 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.cpp
@@ -5,6 +5,7 @@ public:
         int count = words.size();
         for(int i = 0 ; i < words.size() ; i++)
         {
+            // was not getting accepted when words[i] = i and pref = ikw___ hence the condition
             for(int j = 0; j < pref.size() && j < words[i].size() ; j++)
             {
                 if( pref[j] != words[i][j] || words[i].size() < pref.size() )
