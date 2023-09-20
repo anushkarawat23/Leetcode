fabe3ffa00edeab116faab96dd7041676169dc79
Anushka Rawat <112893429+anushkarawat23@users.noreply.github.com>
2023-09-20 16:36:52 +0530

    Time: 2 ms (43.83%), Space: 9.2 MB (5.74%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -21,18 +21,22 @@ public:
         reverse(ans.begin() , ans.end());
         return ans;
 */
+        // Time Complexity: O(N)   Space Complexity: O(N)
+
         vector<int> ans;
         int i = digits.size() - 1;
+        //if the digit is not 9 then we can just increment it
         if( digits[i] != 9 )
             digits[i]++;
-        else                // 999  -> 1000    //7899 -> 7900    //79 -> 80
+
+        else   //if the digit is 9         // 999  -> 1000    //7899 -> 7900    //79 -> 80
         {
             while( digits[i] == 9 && i > 0)
             {
                 digits[i] = 0;
                 i--;
             }
-            if( i == 0 && digits[i] == 9)
+            if( i == 0 && digits[i] == 9)  //to check for numbers like 99, 999, 9
             {
                 ans.push_back(1);
                 digits[i] = 0;
