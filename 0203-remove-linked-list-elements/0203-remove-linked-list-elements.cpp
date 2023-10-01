991fb45d6e00d8074097ee182982c0001c8e96e3
Anushka Rawat <112893429+anushkarawat23@users.noreply.github.com>
2023-10-01 20:36:54 +0530

    Time: 10 ms (97.82%), Space: 15.4 MB (17.07%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
@@ -8,68 +8,37 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
- /*
+ 
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) 
     {
-           //n = 0         n = 1
-        if( !head || (!head -> next && head -> val == val) )
+           
+        if( !head )
             return NULL;
 
         while( head != NULL && head -> val == val )
             head = head -> next;
 
         ListNode* curr = head;
-        ListNode* prev = head;
-        ListNode* after;
+        ListNode* prev = NULL;
+     
         
         while( curr )
         {
-            after = curr -> next;
-            while( curr && curr -> val == val )
+            if( curr -> val == val )
             {
-                after = after -> next;    //wrong line
-                prev -> next = after;
-                curr = after;  
+                prev -> next = curr -> next;
+                curr = curr -> next;
             }
-            prev = curr;
-            curr = after;
-        }
-        return head;
-    }
-};
-*/
-
-class Solution {
-public:
-    ListNode* removeElements(ListNode* head, int val) {
-        // Check for an empty list
-        if (!head) {
-            return nullptr;
-        }
-
-        // Create a dummy node to simplify the removal of the first node if needed
-        ListNode* dummy = new ListNode(0);
-        dummy->next = head;
-
-        ListNode* prev = dummy;
-        ListNode* curr = head;
-
-        while (curr) {
-            if (curr->val == val) {
-                prev->next = curr->next;
-                delete curr; // Free memory for the removed node
-                curr = prev->next;
-            } else {
+            else
+            {
                 prev = curr;
-                curr = curr->next;
+                curr = curr -> next;
             }
         }
-
-        head = dummy->next;
-        delete dummy; // Free memory for the dummy node
-
         return head;
     }
 };
+
+
