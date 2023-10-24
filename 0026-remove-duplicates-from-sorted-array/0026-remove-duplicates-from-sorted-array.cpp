11ba8369cf037ddd2d50eb7866f4c182135f9cce
Anushka Rawat <112893429+anushkarawat23@users.noreply.github.com>
2023-10-24 20:09:42 +0530

    Time: 12 ms (26.34%), Space: 18.9 MB (15.87%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -21,28 +21,25 @@ public:
 };
 */
 
+
 //  OPTIMIZED SOLUTION (TWO POINTERS)
 
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) {
-        if (nums.empty()) {
-            return 0; // If the vector is empty, there are no duplicates to remove.
-        }
-        
-        int idx = 0; // Pointer for the current position in the modified vector.
-        
-        // Iterate through the vector starting from the second element.
-        for (int i = 1; i < nums.size(); i++) {
-            // If the current element is different from the previous element, 
-            // add it to the modified vector at the correct position.
-            if (nums[i] != nums[idx]) {
-                idx++;
-                nums[idx] = nums[i];
+    int removeDuplicates(vector<int>& nums) 
+    {
+        int index = 0;
+        for(int i = 1; i < nums.size() ; i++)
+        {
+            if(nums[index] != nums[i])
+            {
+                index++;
+                nums[index] = nums[i];
             }
         }
-        
-        // Return the new size of the vector after removing duplicates.
-        return idx + 1;
+
+        return index + 1;
     }
+
+        
 };
